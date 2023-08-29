510fa0e74a79c2c8abc54f702ba32e7000de60f5
Usmonbek Ravshanov <usmonbek_ravshanov@epam.com>
2023-08-29 18:42:50 +0500

    init_db was changed

--- init_db.py
+++ init_db.py
@@ -2,8 +2,5 @@ from tools import db
 
 
 def init_db():
-    return
-    from .models.workflow import Prompt
-    from .models.example import Example
-    from .models.variable import Variable
+    from .models.workflow import Workflow
     db.get_shared_metadata().create_all(bind=db.engine)
