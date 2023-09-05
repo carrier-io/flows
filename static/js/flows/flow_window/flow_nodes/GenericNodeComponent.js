ddec709b81fec3ed3a2c0df841c17ac6339c32a8
Aspect13 <ad13box@gmail.com>
2023-09-05 14:38:48 +0400

    fixed generic node to have properties window

--- static/js/flows/flow_window/flow_nodes/GenericNodeComponent.js
+++ static/js/flows/flow_window/flow_nodes/GenericNodeComponent.js
@@ -1,7 +1,19 @@
 const GenericNodeComponent = {
     props: ['node_meta', 'node_data', 'node_id'],
+    data() {
+        return {
+            options: {
+                properties_open: false,
+            }
+        }
+    },
     mounted() {
         Object.assign(this.$data, this.node_data || {})
+        V.registered_components.DrawFlowStuff.editor.on('nodeSelected', id => {
+            if (this.node_id !== id) {
+                this.options.properties_open = false
+            }
+        })
     },
     methods: {
         // attachDataProxy() {
@@ -17,6 +29,7 @@ const GenericNodeComponent = {
         // },
         handleNotify() {
             showNotify('INFO', 'Settings for node')
+            this.options.properties_open = !this.options.properties_open
         }
     },
     template: `
@@ -30,6 +43,26 @@ const GenericNodeComponent = {
             >
                 <i class="icon__18x18 icon-settings"></i>
             </button>
+        </div>
+        <div class="card flow_node_properties_container" 
+            v-if="options.properties_open"
+        >
+            <div class="card-header d-flex">
+                <div class="flex-grow-1">
+                    <p>Properties</p>
+                    <p>{{ node_meta.name }}</p>
+                </div>
+                <div>
+                    <button class="btn btn-action btn-24"
+                        @click="options.properties_open = false"
+                    >
+                        <i class="fa fa-times"></i>
+                    </button>
+                </div>
+                
+            </div>
+            <div class="card-body">
+            </div>
         </div>
     </div>
     `
