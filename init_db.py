a9a80f6614330a15818c905bbab2c5a2cb92dcd1
Aspect13 <ad13box@gmail.com>
2024-04-08 14:51:30 +0400

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -6,7 +6,7 @@ def init_db(rpc_manager):
     from .models.flow import Flow
     from .models.flow_run import FlowRun
     # rpc_manager = rpc_tools.RpcMixin().rpc
-    project_list = rpc_manager.call.project_list()
+    project_list = rpc_manager.call.project_list(filter_={'create_success': True})
     for i in project_list:
         with db.with_project_schema_session(i['id']) as tenant_db:
             db.get_tenant_specific_metadata().create_all(bind=tenant_db.connection())
